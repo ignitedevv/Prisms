6b5d76fe8d505918838a09a66a5b7286aa95314e
ignitedevv <info@ignitedevv.com>
2023-04-18 16:08:26 -0400

    t

--- PrismsApp/urls.py
+++ PrismsApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.LoginUser.as_view(), name='login'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 
 ]
 
